15e62ce07df8663f8b5ab8cc3baab16e4e15c67b
Vincent-Herve <vherve.pro@gmail.com>
2023-02-13 12:29:12 +0100

    add playlist's track list in playlist detail

--- src/app/modules/playlists/components/playlists-detail/playlists-detail.component.ts
+++ src/app/modules/playlists/components/playlists-detail/playlists-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject, tap } from 'rxjs';
 import { SpotifyService } from 'src/app/modules/shared/services/api/spotify.service';
+import { ITrack } from '../interfaces';
 
 @Component({
   selector: 'app-playlists-detail',
@@ -10,6 +11,7 @@ import { SpotifyService } from 'src/app/modules/shared/services/api/spotify.serv
 })
 export class PlaylistsDetailComponent implements OnInit {
   playlist$: Observable<any>;
+  tracks$: ReplaySubject<ITrack[]> = new ReplaySubject();
 
   constructor(
     private spotifyService: SpotifyService,
@@ -22,6 +24,17 @@ export class PlaylistsDetailComponent implements OnInit {
 
   getOnePlaylist(): void {
     const id = this.route.snapshot.params['id'];
-    this.playlist$ = this.spotifyService.getOnePlaylist(id || '');
+    this.playlist$ = this.spotifyService.getOnePlaylist(id || '').pipe(
+      tap((data: any) => {
+        this.tracks$.next(this.mapDataTracksToITrack(data));
+      })
+    );
+  }
+
+  private mapDataTracksToITrack(data: any): ITrack[] {
+    return data.tracks.items.map((item: any) => ({
+      name: item.track.name,
+      uri: item.track.uri,
+    }));
   }
 }
